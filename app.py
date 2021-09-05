c5d829ac0555b326618de6d8f636ac584fec6f3b
daru darmakusuma <darudarmakusuma@gmail.com>
2021-09-06 00:34:21 +0700

    Update web

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import re
+import os
 from flask import Flask, render_template, request
 from crypto.vigenere import Vigenere
 from crypto.fullvigenere import FullVigenere
@@ -42,6 +42,25 @@ def decrypt(cipher, key, method):
     if (method == "vigenere"):
         cipher = cipher.upper()
         plaintext = Vigenere.decrypt(cipher, key)
+    elif (method == "fullvigenere"):
+        cipher = cipher.upper()
+        plaintext = FullVigenere.decrypt(cipher, key)
+    elif (method == "autokeyvigenere"):
+        cipher = cipher.upper()
+        plaintext = AutoVigenere.decrypt(cipher, key)
+    elif (method == "exvigenere"):
+        plaintext = ExVigenere.decrypt(cipher, key)
+    elif (method == "playfair"):
+        cipher = cipher.upper()
+        plaintext = Playfair.decrypt(cipher, key)
+    elif (method == "affine"):
+        cipher = cipher.upper()
+        plaintext = Affine.decrypt(cipher, key[0], key[1])
+    elif (method == "enigma"):
+        cipher = cipher.upper()
+        plaintext = Vigenere.decrypt(cipher, key)
+    else:
+        plaintext = "error"
     return plaintext
 
 @app.route('/')
@@ -65,13 +84,38 @@ def encryptMethod():
     print(cipher)
     return render_template('index.html', output=cipher)
 
+@app.route("/encrypt-file", methods=["POST"])
+def encryptFileMethod():
+    req = request.form
+    print(req)
+    f = request.files['file'] 
+    if f.filename != '': 
+        f.save(f.filename) 
+    file = open(f.filename, "r")
+    text = file.read()
+    method = req["method"]
+    if (method == "affine"):
+        keyA = int(req["keyA"])
+        keyB = int(req["keyB"])
+        key = [keyA, keyB]
+    else:
+        key = req["key"]
+    cipher = encrypt(text,key,method)
+    os.remove(f.filename)
+    return render_template('index.html', output=cipher)
+
 @app.route("/decrypt", methods=["POST"])
 def decryptMethod():
     req = request.form
     print(req)
     text = req["text"]
-    key = req["key"]
     method = req["method"]
+    if (method == "affine"):
+        keyA = int(req["keyA"])
+        keyB = int(req["keyB"])
+        key = [keyA, keyB]
+    else:
+        key = req["key"]
     plaintext = decrypt(text,key,method)
     print(text)
     return render_template('index.html', output=plaintext)
