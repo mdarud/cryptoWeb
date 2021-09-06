f4e56a3580874d75f25dccd7352a1cef74f05acd
daru darmakusuma <darudarmakusuma@gmail.com>
2021-09-06 07:26:46 +0700

    Update web and file handling

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
-from flask import Flask, render_template, request
+from crypto.utils import guessExt
+from flask import Flask, render_template, request, send_file
 from crypto.vigenere import Vigenere
 from crypto.fullvigenere import FullVigenere
 from crypto.autokeyvigenere import AutoVigenere
@@ -12,6 +13,9 @@ app = Flask(__name__,
             static_url_path='',
             static_folder='static',
             template_folder='templates')
+
+filetype = "text"
+
 
 def encrypt(plaintext, key, method):
     if (method == "vigenere"):
@@ -38,6 +42,7 @@ def encrypt(plaintext, key, method):
         cipher = "error"
     return cipher
 
+
 def decrypt(cipher, key, method):
     if (method == "vigenere"):
         cipher = cipher.upper()
@@ -63,12 +68,36 @@ def decrypt(cipher, key, method):
         plaintext = "error"
     return plaintext
 
+
+def methodName(method):
+    if (method == "vigenere"):
+        text = "Vigenere Cipher"
+    elif (method == "fullvigenere"):
+        text = "Full Vigenere Cipher"
+    elif (method == "autokeyvigenere"):
+        text = "Auto Key Vigenere Cipher"
+    elif (method == "exvigenere"):
+        text = "Extended Vigenere Cipher"
+    elif (method == "playfair"):
+        text = "Playfair Cipher"
+    elif (method == "affine"):
+        text = "Affine Cipher"
+    elif (method == "enigma"):
+        text = "Enigma Cipher"
+    else:
+        text = "error"
+    return text
+
+
 @app.route('/')
 def home():
     return render_template('index.html')
 
+
 @app.route("/encrypt", methods=["POST"])
 def encryptMethod():
+    global attc
+    attc = "EncryptedText.txt"
     req = request.form
     print(req)
     text = req["text"]
@@ -79,33 +108,76 @@ def encryptMethod():
         key = [keyA, keyB]
     else:
         key = req["key"]
-    cipher = encrypt(text,key,method)
+    cipher = encrypt(text, key, method)
     print(text)
     print(cipher)
-    return render_template('index.html', output=cipher)
+    method = methodName(method)
+    f = open(attc, "w")
+    f.write(cipher)
+    f.close()
+    global flag
+    flag = 0
+    return render_template('index.html', input=text, method=method+" - Text (Encrypt)", output=cipher)
+
 
 @app.route("/encrypt-file", methods=["POST"])
 def encryptFileMethod():
+    global attc
     req = request.form
     print(req)
-    f = request.files['file'] 
-    if f.filename != '': 
-        f.save(f.filename) 
-    file = open(f.filename, "r")
-    text = file.read()
+    f = request.files['file']
     method = req["method"]
-    if (method == "affine"):
-        keyA = int(req["keyA"])
-        keyB = int(req["keyB"])
-        key = [keyA, keyB]
+    filetype = req["filetype"]
+    if f.filename != '':
+        f.save(f.filename)
+    if (filetype == "text"):
+        file = open(f.filename, "r")
+        text = file.read()
+        method = req["method"]
+        if (method == "affine"):
+            keyA = int(req["keyA"])
+            keyB = int(req["keyB"])
+            key = [keyA, keyB]
+        else:
+            key = req["key"]
+        cipher = encrypt(text, key, method)
+        file.close()
+        method = methodName(method)
+        attc = f.filename.split(".")[0] + "-Encrypted.txt"
+        fi = open(attc, "w")
+        fi.write(cipher)
+        fi.close()
+    elif (filetype == "binary"):
+        file = open(f.filename, "rb")
+        bytes = file.read()
+        text = bytes.decode("utf-8", "ignore")
+        if (method == "affine"):
+            keyA = int(req["keyA"])
+            keyB = int(req["keyB"])
+            key = [keyA, keyB]
+        else:
+            key = req["key"]
+        cipher = encrypt(text, key, method)
+        file.close()
+        method = methodName(method)
+        attc = f.filename.split(".")[0]
+        content = cipher.encode("utf-8", "ignore")
+        fi = open(attc, 'wb')
+        fi.write(content)
+        fi.close()
+        return render_template('index.html', input=text[:100], method=method+" - File (Encrypt)", output=cipher[:100])
     else:
-        key = req["key"]
-    cipher = encrypt(text,key,method)
+        text = "error"
+        cipher = "error"
+        method = methodName(method)
     os.remove(f.filename)
-    return render_template('index.html', output=cipher)
+    return render_template('index.html', input=text, method=method+" - File (Encrypt)", output=cipher)
+
 
 @app.route("/decrypt", methods=["POST"])
 def decryptMethod():
+    global attc
+    attc = "DecryptedText.txt"
     req = request.form
     print(req)
     text = req["text"]
@@ -116,9 +188,79 @@ def decryptMethod():
         key = [keyA, keyB]
     else:
         key = req["key"]
-    plaintext = decrypt(text,key,method)
+    plaintext = decrypt(text, key, method)
     print(text)
-    return render_template('index.html', output=plaintext)
+    method = methodName(method)
+    f = open(attc, "w")
+    f.write(plaintext)
+    f.close()
+    return render_template('index.html', input=text, method=method+" - Text (Decrypt)", output=plaintext)
+
+
+@app.route("/decrypt-file", methods=["POST"])
+def decryptFileMethod():
+    global attc
+    req = request.form
+    print(req)
+    f = request.files['file']
+    method = req["method"]
+    filetype = req["filetype"]
+    if f.filename != '':
+        f.save(f.filename)
+    if (filetype == "text"):
+        file = open(f.filename, "r")
+        text = file.read()
+        method = req["method"]
+        if (method == "affine"):
+            keyA = int(req["keyA"])
+            keyB = int(req["keyB"])
+            key = [keyA, keyB]
+        else:
+            key = req["key"]
+        plaintext = decrypt(text, key, method)
+        file.close()
+        method = methodName(method)
+        attc = f.filename.split(".")[0] + "-Decrypted.txt"
+        fi = open(attc, "w")
+        fi.write(plaintext)
+        fi.close()
+    elif (filetype == "binary"):
+        file = open(f.filename, "rb")
+        bytes = file.read()
+        text = bytes.decode("utf-8", "ignore")
+        if (method == "affine"):
+            keyA = int(req["keyA"])
+            keyB = int(req["keyB"])
+            key = [keyA, keyB]
+        else:
+            key = req["key"]
+        plaintext = decrypt(text, key, method)
+        file.close()
+        method = methodName(method)
+        content = plaintext.encode("utf-8", "ignore")
+        fa = open("temp", 'wb')
+        fa.write(content)
+        fa.close()
+        attc = f.filename + guessExt("temp")
+        print(attc)
+        fi = open(attc, 'wb')
+        fi.write(content)
+        fi.close()
+        return render_template('index.html', input=text[:100], method=method+" - File (Decrypt)", output=plaintext[:100])
+    else:
+        text = "error"
+        cipher = "error"
+        method = methodName(method)
+    os.remove(f.filename)
+    return render_template('index.html', input=text, method=method+" - File (Decrypt)", output=plaintext)
+
+
+@app.route('/download')
+def downloadFile():
+    path = attc
+    print(attc)
+    return send_file(path, as_attachment=True)
+
 
 if __name__ == '__main__':
     app.run()
--- static/js/main.js
+++ static/js/main.js
@@ -23,6 +23,10 @@ $("#decryptFile").click(function() {
   $(this).closest("form").attr("action", "/decrypt-file");       
 });
 
+$("#downloadOutput").click(function() {
+  $(this).closest("form").attr("action", "/download");       
+});
+
 $('.method-select-text').on('change', function() {
   if (this.value == "affine"){
     $('.text-input').html('<div class="col-md-6 form-group mb-3"><label for="" class="col-form-label">Text</label> <input type="text" class="form-control" name="text" id="text" placeholder="Insert text" required/> </div> <div class="col-md-3 form-group mb-3"> <label for="" class="col-form-label">Key a</label> <input type="number" class="form-control" name="keyA" id="keyA" placeholder="Insert key a" required/></div><div class="col-md-3 form-group mb-3"> <label for="" class="col-form-label">Key b</label> <input type="number" class="form-control" name="keyB" id="keyB" placeholder="Insert key b" required/></div>');
